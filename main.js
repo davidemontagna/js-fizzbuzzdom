bea0e953b10070cacd37eb7a576d996eb2dc9ea1
davidemontagna <davidemontagna92@gmail.com>
2021-12-10 16:04:53 +0100

    Fine esercizio

--- main.js
+++ main.js
@@ -1,28 +1,31 @@
 let contenitore = document.getElementById('container')
 
 
-for (let index = 0; index <= 100; index++) { //ciclo for di numeri da 1 a 100
+for (let index = 1; index <= 100; index++) {   //ciclo for di numeri da 1 a 100
     
     let div = document.createElement("div")
     div.className = "box"
     //console.log(index)
     
-    if (index % 3 == 0 && index % 5 == 0){  //controllo divisibilità per 3 e per 5
+    if (index % 3 == 0 && index % 5 == 0){    //controllo divisibilità per 3 e per 5
 
-        div.append("FizzBuzz")
+        div.append("FizzBuzz");
+        div.classList.add('bg-green');    //inserimento colore background
         contenitore.append(div);
         //console.log("FizzBuzz");
         
-    }else if (index % 5 == 0){  //controllo divisibilità per 5
+    }else if (index % 5 == 0){    //controllo divisibilità per 5
 
-        div.append("Buzz")
+        div.append("Buzz");
+        div.classList.add('bg-5');
         contenitore.append(div);
         
         //console.log("Buzz")
 
-    }else if (index % 3 == 0){ //controllo divisibilità per 3
+    }else if (index % 3 == 0){   //controllo divisibilità per 3
 
         div.append("Fizz")
+        div.classList.add('bg-3');
         contenitore.append(div);
         
        // console.log("Fizz")
