f81ab54456eac0f16be00dd144026172407d2066
davidemontagna <davidemontagna92@gmail.com>
2021-12-10 15:36:14 +0100

    centramento scritto e utilizzo di append

--- main.js
+++ main.js
@@ -1,26 +1,37 @@
 let contenitore = document.getElementById('container')
 
+
 for (let index = 0; index <= 100; index++) { //ciclo for di numeri da 1 a 100
     
+    let div = document.createElement("div")
+    div.className = "box"
     //console.log(index)
     
     if (index % 3 == 0 && index % 5 == 0){  //controllo divisibilità per 3 e per 5
 
-        contenitore.innerHTML += '<div class="box">' + 'FizzBuzz' + '</div>';
+        div.append("FizzBuzz")
+        contenitore.append(div);
         //console.log("FizzBuzz");
         
     }else if (index % 5 == 0){  //controllo divisibilità per 5
 
-        contenitore.innerHTML += '<div class="box">' + 'Buzz' + '</div>';
+        div.append("Buzz")
+        contenitore.append(div);
+        
         //console.log("Buzz")
 
     }else if (index % 3 == 0){ //controllo divisibilità per 3
 
-        contenitore.innerHTML += '<div class="box">' + 'Fizz' + '</div>';
+        div.append("Fizz")
+        contenitore.append(div);
+        
        // console.log("Fizz")
 
     }else{
-        contenitore.innerHTML += '<div class="box">' + index + '</div>';
+
+        div.append(index)
+        contenitore.append(div);
+        
         //console.log(index);
     }
 }
