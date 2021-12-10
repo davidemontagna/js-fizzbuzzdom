d424eca5c7f0b1b8d36dcabb0827efaa2017b365
davidemontagna <davidemontagna92@gmail.com>
2021-12-10 15:16:18 +0100

    Posizionamento boxes e inserimento num

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+let contenitore = document.getElementById('container')
 
 for (let index = 0; index <= 100; index++) { //ciclo for di numeri da 1 a 100
     
@@ -6,17 +6,21 @@ for (let index = 0; index <= 100; index++) { //ciclo for di numeri da 1 a 100
     
     if (index % 3 == 0 && index % 5 == 0){  //controllo divisibilità per 3 e per 5
 
-        console.log("FizzBuzz");
+        contenitore.innerHTML += '<div class="box">' + 'FizzBuzz' + '</div>';
+        //console.log("FizzBuzz");
         
     }else if (index % 5 == 0){  //controllo divisibilità per 5
 
-        console.log("Buzz")
+        contenitore.innerHTML += '<div class="box">' + 'Buzz' + '</div>';
+        //console.log("Buzz")
 
     }else if (index % 3 == 0){ //controllo divisibilità per 3
 
-        console.log("Fizz")
+        contenitore.innerHTML += '<div class="box">' + 'Fizz' + '</div>';
+       // console.log("Fizz")
 
     }else{
-        console.log(index);
+        contenitore.innerHTML += '<div class="box">' + index + '</div>';
+        //console.log(index);
     }
 }
