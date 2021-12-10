f6e1c72dae8f97abd14f73d3ff174c67c739322e
davidemontagna <davidemontagna92@gmail.com>
2021-12-10 14:48:07 +0100

    stampa in console delle scritte al posto dei num

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 
 
-for (let index = 0; index <= 100; index++) {
-    console.log(index)
+for (let index = 0; index <= 100; index++) { //ciclo for di numeri da 1 a 100
     
+    //console.log(index)
+    
+    if (index % 3 == 0 && index % 5 == 0){  //controllo divisibilità per 3 e per 5
+
+        console.log("FizzBuzz");
+        
+    }else if (index % 5 == 0){  //controllo divisibilità per 5
+
+        console.log("Buzz")
+
+    }else if (index % 3 == 0){ //controllo divisibilità per 3
+
+        console.log("Fizz")
+
+    }else{
+        console.log(index);
+    }
 }
